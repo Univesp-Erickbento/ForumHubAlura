745da5c2411cc5caa195ecd4fc16decffb7766c3
erickBento <erickbento.univesp@gmail.com>
2025-01-17 14:15:17 -0300

    projeto iniciado

---
+++ src/test/java/com/bento/forumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bento.forumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
